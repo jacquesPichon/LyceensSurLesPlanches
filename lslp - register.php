10e4abca7a911b4045acdf40e0393931f006821a
Damien VERGER <verger.damien35@free.fr>
2018-06-30 15:17:13 +0200

    Ajout des fichiers de plugins

---
+++ lslp - register.php
@@ -0,0 +1,8 @@
+<?php 
+/*
+*Plugin name: Lycéens sur les planches - Inscription
+*Description: Cette extension permettra la gestion des inscriptions des troupes de comédien, pour les représentations du festival de théâtre francophone.
+*Plugin URI: https://github.com/jacquesPichon/LyceensSurLesPlanches
+*License: GPLv2 or later
+*/
+
